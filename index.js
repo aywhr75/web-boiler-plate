edd96ac5fa00772e55e0787ffd186e31aff87190
young <aywhr75@gmail.com>
2020-07-23 09:06:34 -0400

    login page/generate token parts update

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 5000
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const config = require('./config/key');
 
@@ -11,10 +12,11 @@ const {User} = require("./models/User");
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(bodyParser.json());
-
+app.use(cookieParser());
 
 
 const mongoose = require('mongoose')
+
 mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify:false
 }).then(() => console.log('MongoDB Connected...'))
@@ -25,8 +27,7 @@ mongoose.connect(config.mongoURI, {
 app.get('/', (req, res) => res.send('Hello new World!' ))
 
 app.post('/register', (req, res) =>{
-    //회원가입 할 때 필요한 정보들을 client에서 가져오면 그것들을 데이터 베이스에 넣어준다.
-
+    //Put client info into database
     const user = new User(req.body)
 
     user.save((err, userInfo) => {
@@ -36,5 +37,33 @@ app.post('/register', (req, res) =>{
         })
     })
 })
+
+app.post('/login', (req, res)=>{
+//finding requested loggin id in a database
+User.findOne({ email: req.body.email }, (err, user) => {
+    if(!user){
+        return res.json({
+            loginSuccess: false,
+            message: "Cannot find a email!!"
+        })
+    }
+    //if loggin info exist in a database check password
+    user.comparePassword(req.body.password, (err, isMatch) => {
+        if(!isMatch)
+        return res.json({loginSuccess: false, message: "Password is incorrect!!!"})
+
+     //generate token if password info and login info matched
+        user.generateToken((err, user) => {
+            //receive token and check
+            if(err) return res.status(400).send(err);
+            //save token into cookie.
+            res.cookie('x_auth', user.token)
+            .status(200)
+            .json({ loginSuccess: true, userId: user._id })
+        })
+    })
+})
+})
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
     name: {
@@ -30,6 +33,45 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 })
+
+userSchema.pre('save', function(next){
+    var user = this; // userSchema
+
+    if(user.isModified('password')){
+    //encrypting user's password
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if(err) return next(err)
+
+            bcrypt.hash(user.password,salt, function(err, hash){
+                if(err) return next(err)
+                user.password = hash
+                next()
+            })
+        })
+    }else{
+        next()
+    }
+})
+
+userSchema.methods.comparePassword = function(plainPassword, cb){
+    //Plain pw and encrypting pw in database compare
+    //Also, plain pw needs to be encrypted    
+        bcrypt.compare(plainPassword, this.password, function(err, isMatch){
+        if(err) return cb(err)
+        cb(null, isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function(cb){
+    var user = this;
+    //generate token using by jsonwebtoken
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+    user.token = token
+    user.save(function(err, user) {
+        if(err) return cb(err)
+        cb(null, user)
+    })
+}
 
 const User = mongoose.model('User', userSchema)
 
