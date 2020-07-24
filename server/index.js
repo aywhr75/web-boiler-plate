b85c01799a84bc561fa2150b48d4f63ab3933377
young <aywhr75@gmail.com>
2020-07-24 12:01:16 -0400

    Login page/Redux build up

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express()
-const port = 5000
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
@@ -25,6 +24,8 @@ mongoose.connect(config.mongoURI, {
 
 
 app.get('/', (req, res) => res.send('Hello new World!' ))
+
+app.get('/api/hello', (req, res) => res.send('Hello World!~~~~'))
 
 app.post('/api/users/register', (req, res) =>{
     //Put client info into database
@@ -88,5 +89,10 @@ app.get('/api/users/logout', auth, (req, res) => {
             return res.status(200).send({ success: true})
         })
 })
+
+
+
+
+const port = 5000
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
